d1b48453913120bc3457195997747f693bafa9be
Ihab Hussein <ihab@ihabhussein.com>
2018-06-26 13:00:18 +0200

    Use modern syntax

--- ersatz_jquery.js
+++ ersatz_jquery.js
@@ -1,40 +1,40 @@
-$ = function(arg) {
+let $ = arg => {
     if (typeof arg == 'function')
         return window.addEventListener('load', arg);
 
     let _ = (typeof arg == 'string')? document.querySelectorAll(arg): [arg];
     return {
-        on: function(ev, cb) {
+        on: (ev, cb) => {
             _.forEach(el => el.addEventListener(ev, cb));
             return this;
         },
 
-        show: function() {
+        show: () => {
             _.forEach(el => el.style.display = '');
             return this;
         },
 
-        hide: function() {
+        hide: () => {
             _.forEach(el => el.style.display = 'none');
             return this;
         },
 
-        css: function(k, v) {
+        css: (k, v) => {
             if (typeof k != 'undefined') _.forEach(el => el.style[k] = v);
             return _[0].style[k];
         },
 
-        text: function(v) {
+        text: v => {
             if (typeof v != 'undefined') _.forEach(el => el.innerText = v);
             return _[0].innerText;
         },
 
-        val: function(v) {
+        val: v => {
             if (typeof v != 'undefined') _.forEach(el, el.value = v);
             return _[0].value;
         },
 
-        ajax: function(url, settings) {
+        ajax: (url, settings) => {
             let opt = Object.assign({
                 method: 'GET', data: null,
             }, settings || {});
