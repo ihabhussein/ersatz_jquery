14fd7dab44108eb838c76875f94dc9485e9e7508
Ihab Hussein <ihab@ihabhussein.com>
2018-07-28 20:04:17 +0200

    Implement robust call chaining

--- ersatz_jquery.js
+++ ersatz_jquery.js
@@ -1,37 +1,45 @@
-let $ = arg => {
-    if (typeof arg == 'function')
-        return window.addEventListener('load', arg);
+let $ = a => {
+    if (typeof a == 'function') return window.addEventListener('load', a);
 
-    let _ = (typeof arg == 'string')? document.querySelectorAll(arg): [arg];
+    let _ = (typeof a == 'string')? document.querySelectorAll(a): a;
     return {
         on: (ev, cb) => {
             _.forEach(el => el.addEventListener(ev, cb));
-            return this;
+            return $(_);
         },
 
         show: () => {
             _.forEach(el => el.style.display = '');
-            return this;
+            return $(_);
         },
 
         hide: () => {
             _.forEach(el => el.style.display = 'none');
-            return this;
+            return $(_);
         },
 
         css: (k, v) => {
-            if (typeof k != 'undefined') _.forEach(el => el.style[k] = v);
-            return _[0].style[k];
+            if (typeof k == 'undefined') return _[0].style[k];
+            _.forEach(el => el.style[k] = v);
+            return $(_);
         },
 
         text: v => {
-            if (typeof v != 'undefined') _.forEach(el => el.innerText = v);
-            return _[0].innerText;
+            if (typeof v == 'undefined') return _[0].innerText;
+            _.forEach(el => el.innerText = v);
+            return $(_);
+        },
+
+        html: v => {
+            if (typeof v == 'undefined') return _[0].innerHTML;
+            _.forEach(el => el.innerHTML = v);
+            return $(_);
         },
 
         val: v => {
-            if (typeof v != 'undefined') _.forEach(el, el.value = v);
-            return _[0].value;
+            if (typeof v == 'undefined') return _[0].value;
+            _.forEach(el, el.value = v);
+            return $(_);
         },
     };
 };
