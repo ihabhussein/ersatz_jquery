2628de6616b981107932e9d6fae461868df84f77
Ihab Hussein <ihab@ihabhussein.com>
2018-07-28 19:29:41 +0200

    Remove ajax
    Fetch API is currently widely supported

--- ersatz_jquery.js
+++ ersatz_jquery.js
@@ -33,27 +33,5 @@ let $ = arg => {
             if (typeof v != 'undefined') _.forEach(el, el.value = v);
             return _[0].value;
         },
-
-        ajax: (url, settings) => {
-            let opt = Object.assign({
-                method: 'GET', data: null,
-            }, settings || {});
-
-            return new Promise((resolve, reject) => {
-                let xhr = new XMLHttpRequest();
-                xhr.withCredentials = true;
-                xhr.addEventListener("load", () => {
-                    let data = {
-                        status: xhr.status,
-                        statusText: xhr.statusText,
-                        result: xhr.response? JSON.parse(xhr.response): null,
-                    };
-                    xhr.status >= 200 && xhr.status < 300? resolve(data): reject(data);
-                });
-                xhr.addEventListener("error", reject);
-                xhr.open(opt.method, url);
-                xhr.send(opt.data);
-            });
-        },
     };
 };
